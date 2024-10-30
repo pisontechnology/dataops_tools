9bec3e98abaa42cdc50eb8cce26353c1bc65d8c5
tvu-pison <tue.vu@pison.com>
2024-10-30 11:15:33 -0400

    modified audio task

--- auditory_change_task/psychopy/tests/test_experiment/test_routines/test_PhotodiodeValidationRoutine.py
+++
@@ -1,9 +0,0 @@
-from . import _TestDisabledMixin, _TestBaseStandaloneRoutinesMixin
-from psychopy import experiment
-from psychopy.experiment.routines.photodiodeValidator import PhotodiodeValidatorRoutine
-
-
-class TestEyetrackerCalibrationRoutine(_TestBaseStandaloneRoutinesMixin, _TestDisabledMixin):
-    def setup_method(self):
-        self.exp = experiment.Experiment()
-        self.rt = PhotodiodeValidatorRoutine(exp=self.exp, name="testPhotodiodeValidatorRoutine")
